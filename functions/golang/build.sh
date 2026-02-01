2afd7bf3f1379daadd3b40c1a76529099b614083
林宇 <linyu.aont@bytedance.com>
2026-02-01 19:29:55 +0800

    Revert changes - restore to working state

--- functions/golang/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-export PATH=/usr/local/go/bin:$PATH
-export GOPROXY=https://goproxy.byted.org,https://goproxy.cn,direct
-export GOSUMDB=sum.golang.google.cn
-go mod download
-go mod verify
-go build -o bootstrap ./init.go
